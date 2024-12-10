3201787f876deb64cbcafe20228d9ecacd8e0aa5
Ank-pro <ank.pro0115@gmail.com>
2024-12-10 19:38:38 +0530

    sidebar

---
+++ gpt-clone/src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import "../App.css";
+import openImg from "../assets/open.png";
+import searchImg from "../assets/search.png";
+import chatImg from "../assets/newChat.png";
+
+export function SideBar({ setShowSidebar ,isOpen }) {
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : 'close'}`}>
+      <div className="top">
+        <img 
+          className="open-bar" 
+          src={openImg} 
+          alt="" 
+          onClick={() => setShowSidebar(false)} 
+        />
+        <div className="top-1">
+          <img className="search" src={searchImg} alt="" />
+          <img className="new-chat" src={chatImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
